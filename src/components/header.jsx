517fd947ca5e0b3214cc66941c1e0a408d5fb043
francotalles <francotallesdev@gmail.com>
2025-06-07 16:25:30 -0400

    adicionando o tailwind

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Header() {
+    const [variavel, setVariavel] = useState();
+
+  return (
+    <>
+      <header>Vasco é foda</header>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,10 @@
+function Sidebar() {
+
+  return (
+    <>
+      <h1>Bar Norte</h1>
+    </>
+  )
+}
+
+export default Sidebar
