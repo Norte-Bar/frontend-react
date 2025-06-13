ea1ea866d402585ba73331a458d336820122adc0
alejandro_ngz <alejandronoguera465@gmail.com>
2025-06-13 11:09:42 -0400

    terminei o sobre

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={
           <>
-            <Reserva />
+            {/* <Reserva /> */}
             <Sobre /> 
           </>
         } />
